56963ff792ba3cbc04dc12c48b4635ccbc0adb4e
Craig Newcomb <craignewcomb@craigs-imac.local>
2018-09-13 15:00:55 -0500

    added keyboard customizations inside custom text view class instead of manually like lecture

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -24,6 +24,8 @@ class CurrencyTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        keyboardType = .decimalPad
+        keyboardAppearance = .dark
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
