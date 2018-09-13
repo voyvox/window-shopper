430432b0f2e815313113753c8333c94808ae29cd
Craig Newcomb <craignewcomb@craigs-imac.local>
2018-09-13 14:50:11 -0500

    built interface for MainVC, used custom class for text fields, made that class @IBDesignable to update storyboard with changes, added accessory view calculate button for when summoning the number pad to fill text views

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Craig Newcomb on 9/13/18.
+//  Copyright © 2018 Craig Newcomb. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        
+    }
+
+}
+
---
+++ window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Craig Newcomb on 9/13/18.
+//  Copyright © 2018 Craig Newcomb. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrencyTextField: UITextField {
+
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 0.9953911901, green: 0.9881951213, blue: 1, alpha: 1)
+        }
+    }
+    
+}
